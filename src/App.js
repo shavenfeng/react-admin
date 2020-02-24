3eeb84438cb5fb8dd77ea332ad6e07829b40965e
feng <feng@ShavendeMacBook-Pro.local>
2020-02-24 12:58:28 +0800

    added admin,login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import 'antd/dist/antd.less'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Admin}></Route>
+        <Route path="/login" component={Login}></Route>
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/admin/admin.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class admin extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  render() {
+    return <div>admin page</div>
+  }
+}
+
+export default admin
