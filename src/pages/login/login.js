7c348bd5e1ed323b9d0a7f1bc406e8b2956669f5
feng <feng@fengdeMacBook-Pro.local>
2020-02-25 11:30:54 +0800

    udpated

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react'
 import './login.less'
-import { Form, Input, Icon,Button } from 'antd'
+import { Form, Input, Icon, Button } from 'antd'
 
 class Login extends Component {
   constructor(props) {
     super(props)
     this.state = {}
   }
+  handleSubmit = e => {
+    e.preventDefault()
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values)
+      }
+    })
+  }
+  passwordValidator = (rule, value, callback) => {
+    if (!value) {
+      callback('密码必须输入')
+    } else if (value.length < 4) {
+      callback('密码长度不能小于4位')
+    } else if (value.length > 12) {
+      callback('密码长度不能大于12位')
+    } else if (!/^[a-zA-Z0-9_]+$/.test(value)) {
+      callback('密码必须是英文、数字或下划线组成')
+    } else {
+      callback() // 验证通过
+    }
+  }
   render() {
+    const { getFieldDecorator } = this.props.form
     return (
       <div className="login-container">
         <header className="login-header">
@@ -17,14 +39,33 @@ class Login extends Component {
           <h2>用户登陆</h2>
           <Form onSubmit={this.handleSubmit} className="login-form">
             <Form.Item>
+              {getFieldDecorator('username', {
+                rules: [
+                  {
+                    required: true,
+                    whitespace: true,
+                    message: '用户名必须输入'
+                  },
+                  { min: 4, message: '用户名至少4位' },
+                  { max: 12, message: '用户名最多12位' },
+                  {
+                    pattern: /^[a-zA-Z0-9_]+$/,
+                    message: '用户名必须是英文、数字或下划线组成'
+                  }
+                ]
+              })(
                 <Input
                   prefix={
                     <Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />
                   }
                   placeholder="Username"
                 />
+              )}
             </Form.Item>
             <Form.Item>
+              {getFieldDecorator('password', {
+                rules: [{ validator: this.passwordValidator }]
+              })(
                 <Input
                   prefix={
                     <Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />
@@ -32,6 +73,7 @@ class Login extends Component {
                   type="password"
                   placeholder="Password"
                 />
+              )}
             </Form.Item>
             <Form.Item>
               <Button
@@ -48,5 +90,5 @@ class Login extends Component {
     )
   }
 }
-
-export default Login
+const WrapLogin = Form.create()(Login)
+export default WrapLogin
